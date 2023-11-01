6f6d17149fb077d8625e90285014e7a029645837
mazurluc <108153342+mazurluc@users.noreply.github.com>
2023-11-01 17:44:58 -0300

    fffff

--- css/main.js
+++ css/main.js
@@ -12,4 +12,6 @@ document.querySelector('.tecla_clap').onclick = TocaSomClap;
 function TocaSomTim(){
     document.querySelector('#som_tecla_tim').play()
     document.querySelector('.tecla_tim').onclick =TocaSomTim;
-    
+    function TocaSomPuff(){
+        document.querySelector('#som_tecla_puff').play()
+        document.querySelector('Tecla_puff').onclick =TocaSomPuff;
